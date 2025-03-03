32691749fb7e5ae361095bb3f0445d83f0932dc7
blstgr <bullsh1tugur@gmail.com>
2025-03-03 21:52:17 +0200

    fixes

--- open-modal.js
+++ open-modal.js
@@ -1,3 +1,7 @@
 document.querySelector('.contact-form').addEventListener('click', () => {
   document.querySelector('.modal-overlay').classList.add('is-open');
 });
+
+document.querySelector('.modal-close').addEventListener('click', () => {
+  document.querySelector('.modal-overlay').classList.remove('is-open');
+});
