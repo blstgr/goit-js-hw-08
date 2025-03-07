e11577cbe5094175f357854443aa65a75a9c906f
blstgr <bullsh1tugur@gmail.com>
2025-03-07 10:39:26 +0200

    hw-06

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# goit-markup-hw-05
+# goit-markup-hw-06
 
-Demo link - https://blstgr.github.io/goit-markup-hw-05
+Demo link - https://blstgr.github.io/goit-markup-hw-06
--- open-modal.js
+++ open-modal.js
@@ -5,3 +5,13 @@ document.querySelector('.contact-form').addEventListener('click', () => {
 document.querySelector('.modal-close').addEventListener('click', () => {
   document.querySelector('.modal-overlay').classList.remove('is-open');
 });
+
+document.querySelector('.burger-icon').addEventListener('click', () => {
+  document.querySelector('.mobile-nav-sidebar').classList.toggle('is-open');
+});
+
+document
+  .querySelector('.mobile-nav-sidebar-close')
+  .addEventListener('click', () => {
+    document.querySelector('.mobile-nav-sidebar').classList.remove('is-open');
+  });
