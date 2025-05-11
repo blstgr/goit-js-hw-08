db8f4841be7e84c5bb084727c50678316aaa25b4
blstgr <bullsh1tugur@gmail.com>
2025-05-12 00:22:55 +0300

    hw08

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-const defaultText = output.innerText;
-
-input.addEventListener('input', event => {
-  const inputValue = event.currentTarget.value.trim();
-  output.textContent = inputValue || defaultText;
-});
